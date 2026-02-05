f1815491186c163d0d16b1ae2057ae8d67730bd8
Toxic <gyaneshp606@gmail.com>
2026-02-05 12:31:50 +0530

    Iam-Toxicc

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 # ───────── BOT ─────────
-BOT_TOKEN = "PASTE_YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "8368367439:AAFBRu_H2Pe2A_uFHf4CtPLlnkPzpKhuNJ4"
 
 # ───────── USER ACCOUNT API ─────────
-API_ID = 123456        # my.telegram.org se
-API_HASH = "API_HASH_HERE"
+API_ID = "28284695"        # my.telegram.org se
+API_HASH = "2fedf2c683513b853cba01854a15d12b"
 
 # ───────── OWNER ─────────
-OWNER_ID = 123456789   # apni telegram numeric id
+OWNER_ID = "8299512910"   # apni telegram numeric id
 
 # ───────── SUPPORT / UI LINKS ─────────
-SUPPORT_GROUP = "https://t.me/your_support_group"
-SUPPORT_CHANNEL = "https://t.me/your_support_channel"
-OWNER_LINK = "https://t.me/your_username"
+SUPPORT_GROUP = "https://t.me/toxiccpro"
+SUPPORT_CHANNEL = "https://t.me/isha_bots"
+OWNER_LINK = "https://t.me/Bhool_Bhulaiya_0"
 
 # ───────── MEDIA FILES ─────────
 START_VIDEO = "media/start.mp4"     # /start intro video
